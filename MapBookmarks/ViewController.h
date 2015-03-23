18c423e7eb7e479ad5d00e8ae782adadab179386
Sergey Voinov <SVoinov@luxoft.com>
2015-03-23 13:19:51 +0200

    Initial Commit

---
+++ MapBookmarks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapBookmarks
+//
+//  Created by Sergey Voinov on 3/23/15.
+//  Copyright (c) 2015 Sergey Voinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MapBookmarks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MapBookmarks
+//
+//  Created by Sergey Voinov on 3/23/15.
+//  Copyright (c) 2015 Sergey Voinov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
